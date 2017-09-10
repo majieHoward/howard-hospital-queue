e592d90b9ef3bc9fabdab9b716a54bc905113bf1
majieHoward <120234740@qq.com>
2017-09-10 17:45:59 +0800

    添加新文件

---
+++ src/main/resources/static/js/consultationRoom/consultationRoom-P1931.js
@@ -0,0 +1,111 @@
+﻿var stompClient = null;
+
+function connect() {	
+	/**
+     * 向服务器发送一个ping包
+     */
+	var pingToServer = function () {
+        String.prototype.getBytes = function() {
+            var bytes = [];
+            for(var i=0;i < this.length;i++){
+                var charCode=this.charCodeAt(i);
+                var cLen=Math.ceil(Math.log(charCode)/Math.log(256));
+                for(var j=0;j < cLen;j++){
+					bytes.push((charCode << (j*8))&0xFF);
+                }
+            }
+            return bytes;
+        };
+
+        var payload = 'i';
+        var buffer = new ArrayBuffer(payload.length);
+        var intView = new Int8Array(buffer);
+        for(var i = 0; i < intView.length; i++) {
+            intView[i] = payload.getBytes()[i];
+        }
+        stompClient.send("/app/messageForwarding.websocket", {}, JSON.stringify({'name': "hreatkeep","device":"127.0.0.1"}));
+	    };
+   
+     /**
+     * 心跳检测
+     * 若30秒内没有接收到任何来自服务器的信息，则向服务器发起一个ping包
+     * @type {{timeout: number, timeoutObj: null, serverTimeoutObj: null, reset: reset, start: start}}
+     */
+    var heartCheck = {
+        timeout: 20000, //计时器设定为20s
+        timeoutObj: null,
+        serverTimeoutObj: null,
+        reset: function() {
+            clearTimeout(this.timeoutObj);
+            clearTimeout(this.serverTimeoutObj);
+            this.start();
+        },
+        start: function() {
+            var self = this;
+            this.timeoutObj = setTimeout(function() {
+                //向服务器发送ping消息
+                pingToServer();
+                //计算答复的超时时间
+                self.serverTimeoutObj = setTimeout(function() {
+                    //如果调用onclose会执行reconnect，导致重连两次，因此直接调用close()关闭连接
+                    //socket.close();
+                }, self.timeout);
+            }, this.timeout);
+        }
+    };
+	//var sockjs = new SockJS(url, _reserved, options);
+	//sockjs.min.js:27 Uncaught Error: Only basic urls are supported in SockJS
+	var socket = new SockJS('/hospitalQueue');
+	//建立一个基于Web Socket的连接到STOMP代理
+	//Simple (or Streaming) Text Orientated Messaging Protocol
+	//STOMP即Simple (or Streaming) Text Orientated Messaging Protocol,简单(流)文本定向消息协议
+	//http://jmesnil.net/stomp-websocket/doc/
+    stompClient = Stomp.over(socket);
+    //client.connect(login, passcode, connectCallback);
+    //client.connect(login, passcode, connectCallback, errorCallback);
+    //client.connect(login, passcode, connectCallback, errorCallback, host);
+    //传递参数
+    var connectionParameter={
+    	"screenDevice": "127.0.0.1"
+    };
+    
+    stompClient.connect(connectionParameter, function (frame) {
+        console.log('Connected:' + frame);
+        console.log("execute function socket.onopen 心跳检测启动");
+	    //heartCheck.start();
+	    stompClient.subscribe('/callTheName/demo', function (response) {
+	    	// and acknowledge it
+	    	 //heartCheck.reset();
+	    	console.log(response);
+	        response.ack();
+	    	
+	    });
+    },function(error){
+    	console.log(error);
+    });
+    //当无法连接到服务器(测试时模拟了服务器DOWN机)
+    socket.onclose = function() {
+	    console.log('execute function socket.onclose connection closed Disconnected from WebSocket server. It will reconnect after 10 seconds...');
+	    // 10秒后重新连接，实际效果：每10秒重连一次，直到连接成功
+        setTimeout(function () {
+        	connect();
+        }, 10000);
+	};
+}
+
+function disconnect() {
+    if (stompClient != null) {
+        stompClient.disconnect();
+    }
+    console.log('Disconnected');
+}
+
+function sendName() {
+    var name = $('#name').val();
+    var device = $('#device').val();
+    stompClient.send("/app/messageForwarding.websocket", {}, JSON.stringify({'name': name,'device':device}));
+}
+
+function showResponse(message) {
+    $("#response").html(message);
+}
---
+++ src/main/resources/static/js/interrogationSpot/interrogationSpot-P1932.js
@@ -0,0 +1,136 @@
+﻿//ready事件：
+//ready事件在DOM结构绘制完成之后就绘执行。
+//这样能确保就算有大量的媒体文件没加载出来，JS代码一样可以执行。
+//load事件：
+//load事件必须等到网页中所有内容全部加载完毕之后才被执行。
+//如果一个网页中有大量的图片的话，则就会出现这种情况：
+//网页文档已经呈现出来，但由于网页数据还没有完全加载完毕，
+//导致load事件不能够即时被触发。
+/**网页可见区域宽:document.body.clientWidth**/
+/**网页可见区域高:document.body.clientHeight**/
+/**网页可见区域宽:document.body.offsetWidth (包括边线的宽)**/
+/**网页可见区域高:document.body.offsetHeight (包括边线的高)**/
+/**网页正文全文宽:document.body.scrollWidth**/
+/**网页正文全文高:document.body.scrollHeight**/
+/**网页被卷去的高:document.body.scrollTop**/
+/**网页被卷去的左:document.body.scrollLeft**/
+/**网页正文部分上:window.screenTop**/
+/**网页正文部分左:window.screenLeft**/
+/**屏幕分辨率的高:window.screen.height**/
+/**屏幕分辨率的宽:window.screen.width**/
+/**屏幕可用工作区高度:window.screen.availHeight**/
+var stompClient = null;
+function setConnected(connected) {
+   
+}
+//$(function(){});
+$(document).ready( function (){
+	disconnect();
+    /**屏幕的宽度 1024**/
+	var screenWidth=document.documentElement?document.documentElement.clientWidth:document.body.clientWidth;
+	/**屏幕的高度 768**/
+	var screenHeight=document.documentElement?document.documentElement.clientHeight:document.body.clientHeight;
+	console.log("screenHeight:"+screenHeight);
+    console.log("screenWidth:"+screenWidth);
+	
+	var logoImglength=48*screenWidth/1024;
+	
+	$("#hospitalLogoImg").css("height",logoImglength+"px");
+	$("#hospitalLogoImg").css("width",logoImglength+"px");
+	var hospitalNameFontSize= 4*screenWidth/1024;
+	$("#hospitalName").css("font-size",hospitalNameFontSize+"rem");
+	
+	var waitingAreaWidth=15*screenWidth/1024;
+	$("#waitingArea").css("padding",waitingAreaWidth+"px");
+	var waitingAreaFontSize=2*screenWidth/1024;
+	$("#waitingArea").css("font-size",waitingAreaFontSize+"rem");
+
+    var waitingAreaFooterHeight=$("#waitingAreaFooter").outerHeight();
+	/**padding: 10px;**/
+	/**margin-bottom: 20px;**/
+    /**font-size:2rem;**/
+	$("#callMessage").css("padding",10*screenHeight/1024+"px");
+	$("#callMessage").css("margin-bottom",20*screenHeight/1024+"px");
+	$("#consultingRoomList").css("margin-top",-20*screenHeight/1024+"px");
+	$("#callMessage").css("font-size",2*screenWidth/1024+"rem");
+    var pageLogoHeight=$('#pageLogo').outerHeight();
+	
+	$('#mainHospitalqueueV').css("padding-top",pageLogoHeight+"px");
+	$("#mainHospitalqueueV").css("vertical-align","middle");
+	//设置bootstrapTable起始的高度
+	var callContentsHeight=$("#callContents").outerHeight();
+	var windowHeight=screenHeight-callContentsHeight-pageLogoHeight-waitingAreaFooterHeight;
+	var trHeight=windowHeight/7;
+	$("#mainHospitalqueueV").find("tr").each(function(){
+		$(this).css("height",trHeight+"px");
+	});
+	$("#mainHospitalqueueV").find("th").each(function(){
+	    //    vertical-align: middle;
+		$(this).css("vertical-align","middle");
+	});
+	$('#consultingRoom').bootstrapTable({height:windowHeight}); 
+});
+$(window).load(function() {
+	connect();
+});
+function disconnect() {
+    if (stompClient != null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log('Disconnected');
+}
+function connect() {
+	//var sockjs = new SockJS(url, _reserved, options);
+	//sockjs.min.js:27 Uncaught Error: Only basic urls are supported in SockJS
+    //建立一个基于Web Socket的连接到STOMP代理
+    //STOMP JavaScript客户端将使用URL为 ws:// 与STOMP server建立通信
+	var socket = new SockJS('/hospitalQueue');
+    stompClient = Stomp.over(socket);
+    //client.connect(login, passcode, connectCallback);
+    //client.connect(login, passcode, connectCallback, errorCallback);
+    //client.connect(login, passcode, connectCallback, errorCallback, host);
+    // 心跳检测heart-beating
+    //如果STOMP 代理接收的帧是STOMP 1.1协议时，默认心跳检测是开启的。
+    //客户端client对象有一个字段 heartbeat ,通过改变incoming和outgoing数值，来配置心跳频率（默认频率值为：10000ms）
+	//客户端每20000ms发送一次心跳检测
+    //client.heartbeat.outgoing = 20000; 
+	//client不接收serever端的心跳检测
+	//client.heartbeat.incoming = 0; 
+	//heart-beating是利用window.setInterval()去规律地发送heart-beats或者检查服务端的heart-beats
+    //传递参数
+    var connectionParameter={
+    	"screenDevice": "SD987JH"
+    }
+    
+    stompClient.connect(connectionParameter, function (frame) {
+        setConnected(true);
+        console.log('Connected:' + frame);
+        stompClient.subscribe('/callTheName/public', function (response) {
+        	// and acknowledge it
+            response.ack();
+        	console.log(response.body);
+        	 var success = function() {  };
+             var error = function(message) {
+             console.log(message);
+             alert("Oopsie! " + message);
+             };
+             TTSClientPlugin.speak(response.body,success,error);
+        });
+        stompClient.subscribe('/screenDevice/171.19.231.2/subscribe', function (response) {
+        	// and acknowledge it
+            response.ack();
+        	console.log(response.body);
+        	 var success = function() {  };
+             var error = function(message) {
+             console.log(message);
+             alert("Oopsie! " + message);
+             };
+             TTSClientPlugin.speak(response.body,success,error);
+        });
+    });
+    //当无法连接到服务器(测试时模拟了服务器DOWN机)
+    socket.onclose = function() {
+	    console.log('execute socket.onclose connection closed');
+	};
+}
